02f11caedebb75ec2e327ee727b798fea1660aa7
xuan1106 <spu96508@gmail.com>
2021-08-22 15:04:38 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,7 +5,6 @@ const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 const db = require('../../models')
-const Todo = db.Todo
 const User = db.User
 
 router.get('/login', (req, res) => {
@@ -23,10 +22,29 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已經註冊過了'})
       return res.render('register', {
+        errors,
         name,
         email,
         password,
