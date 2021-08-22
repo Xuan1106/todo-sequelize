38a523c095a25df14c5ed428bd8505d2de352e09
xuan1106 <spu96508@gmail.com>
2021-08-22 12:41:55 +0800

    feat: add detail page and modify route

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
+
 router.get('/new', (req, res) => {
   res.send('Add new todo')
 })
 
 router.post('/new', (req, res) => {
 
+})
+
+router.get('/:id', (req,res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 router.get(':id/edit', (req,res) => {
