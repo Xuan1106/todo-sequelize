a9826b5a0d3e981849aba00f7d359ac795e1999f
xuan1106 <spu96508@gmail.com>
2021-08-22 12:41:14 +0800

    feat: add passport

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+
+usePassport(app)
 
 app.use(routes)
 
